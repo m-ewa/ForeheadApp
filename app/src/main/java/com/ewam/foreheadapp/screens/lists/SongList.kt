914aefe7a945cf5a653d3bfd7f9630b25f0bebfd
Ewa Modzelewska <ewamodze@gmail.com>
2021-11-05 13:09:27 +0100

    update songList

--- app/src/main/java/com/ewam/foreheadapp/screens/lists/SongList.kt
+++ app/src/main/java/com/ewam/foreheadapp/screens/lists/SongList.kt
@@ -1,19 +1,19 @@
 package com.ewam.foreheadapp.screens.lists
 
 val songList = mutableListOf(
-    "Californication by Red Hot Chili Peppers",
-    "The Queen Is Dead by The Smiths",
-    "Wish You Were Here by Pink Floyd",
-    "London Calling by The Clash",
-    "Back In Black by AC/DC",
-    "Smells Like Teen Spirit by Nirvana",
-    "Imagine by John Lennon",
-    "One by U2",
-    "Billie Jean by Michael Jackson",
-    "Bohemian Rhapsody by Queen",
-    "Hey Jude by The Beatles",
-    "Like A Rolling Stone by Bob Dylan",
-    "I Can't Get No Satisfaction by Rolling Stones",
-    "Wannabe by Spice Girls",
-    "Bad Romance by Lady Gaga"
+    "Californication \nby Red Hot Chili Peppers",
+    "The Queen Is Dead \nby The Smiths",
+    "Wish You Were Here \nby Pink Floyd",
+    "London Calling \nby The Clash",
+    "Back In Black \nby AC/DC",
+    "Smells Like Teen Spirit \nby Nirvana",
+    "Imagine \nby John Lennon",
+    "One \nby U2",
+    "Billie Jean \nby Michael Jackson",
+    "Bohemian Rhapsody \nby Queen",
+    "Hey Jude \nby The Beatles",
+    "Like A Rolling Stone \nby Bob Dylan",
+    "I Can't Get No Satisfaction \nby Rolling Stones",
+    "Wannabe \nby Spice Girls",
+    "Bad Romance \nby Lady Gaga"
 )
